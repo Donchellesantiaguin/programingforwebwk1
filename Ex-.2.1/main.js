b55fb54af428779f50c35d20aa44166ed7d15ac1
Donchellesantiaguin <166774639+Donchellesantiaguin@users.noreply.github.com>
2025-01-30 20:11:42 -0800

    Add files via upload

---
+++ Ex-.2.1/main.js
@@ -0,0 +1,7 @@
+const movies = ["The Devil Wears Prada", "The Princess Diaries", "Ice Princess", "Barbie"];
+// prompt
+let input = prompt("What is your favorite movie?");
+// add input
+movies.push(input);
+// log
+console.log(movies.toString())
